4f78af3e266cd5f2ac8324461dc653637164e913
Jordan Webb <jordan@getseam.com>
2021-01-26 17:22:39 -0600

    Proofread rundir.py

--- podracer/rundir.py
+++ podracer/rundir.py
@@ -10,9 +10,9 @@ RUNROOT = Path('/run/podracer')
 
 @contextmanager
 def podracer_lockfile(name):
-  RUNROOT.mkdir(mode=0o700, parents=True, exist_ok=True)
+  RUNROOT.mkdir(mode=0o755, parents=True, exist_ok=True)
   lockfile = RUNROOT.joinpath(f'{name}.lock')
-  lockfd = os.open(lockfile, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+  lockfd = os.open(lockfile, os.O_CREAT | os.O_WRONLY)
 
   try:
     fcntl.lockf(lockfd, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -27,7 +27,7 @@ def podracer_rundir(name):
   rundir = RUNROOT.joinpath(name)
   with podracer_lockfile(name):
     try:
-      rundir.mkdir(mode=0o755, parents=True)
+      rundir.mkdir(mode=0o700, parents=True)
       yield rundir
     finally:
       shutil.rmtree(rundir)
