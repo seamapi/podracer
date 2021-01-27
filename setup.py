9be9e5436b1b588779fb91843e161893c9b9e390
Jordan Webb <jordan@getseam.com>
2021-01-27 14:58:25 -0600

    Add repack entry point

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ setuptools.setup(
   author_email="hello@getseam.com",
   description="Tool to run repacked containers",
   url="https://github.com/hello-seam/podracer",
-  packages=setuptools.find_packages(),
+  packages=['podracer'],
   python_requires='>=3.8',
   license='AGPL-3.0-or-later',
   entry_points = {
-    'console_scripts': [ 'podracer-run=podracer.run:main' ]
+    'console_scripts': [
+      'podracer-run=podracer.run:main',
+      'podracer-repack=podracer.repack:main',
+    ]
   }
 )
