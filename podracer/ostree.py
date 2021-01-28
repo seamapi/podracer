fd3d86bc1855d1f87edab9b1af3331e7502c7451
Jordan Webb <jordan@getseam.com>
2021-01-28 10:17:37 -0600

    Move PODRACER_LIBDIR into paths

--- podracer/ostree.py
+++ podracer/ostree.py
@@ -1,9 +1,8 @@
 import subprocess
 
 from pathlib import Path
+from podracer.paths import PODRACER_LIBDIR
 from podracer.capture import capture_output
-
-CHECKOUTROOT = Path('/var/lib/podracer/ostree')
 
 
 def ostree_rev_parse(ref):
@@ -13,9 +12,10 @@ def ostree_rev_parse(ref):
 def ostree_checkout(ref):
   sha = ostree_rev_parse(ref)
 
-  CHECKOUTROOT.mkdir(mode=0o700, parents=True, exist_ok=True)
-  checkout = CHECKOUTROOT.joinpath(sha)
+  checkout_root = PODRACER_LIBDIR.joinpath('ostree')
+  checkout_root.mkdir(mode=0o755, parents=True, exist_ok=True)
 
+  checkout = checkout_root.joinpath(sha)
   if not checkout.is_dir():
     subprocess.run(['ostree', 'checkout', sha, str(checkout)], check=True)
 
--- podracer/paths.py
+++ podracer/paths.py
@@ -3,3 +3,4 @@ import os
 from pathlib import Path
 
 PODRACER_RUNDIR = Path(os.environ.get('PODRACER_RUNDIR', '/run/podracer'))
+PODRACER_LIBDIR = Path(os.environ.get('PODRACER_LIBDIR', '/var/lib/podracer'))
